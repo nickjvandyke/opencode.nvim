1e31bbcea06966c004eb3b41e54e1c74136227c8
Megatherium <852741+Megatherium@users.noreply.github.com>
2026-02-19 14:46:09 +0100

    fix(provider): more reliable autocmd for calling stop on exit

--- plugin/provider.lua
+++ plugin/provider.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_create_autocmd("VimLeave", {
+vim.api.nvim_create_autocmd("VimLeavePre", {
   group = vim.api.nvim_create_augroup("OpencodeProvider", { clear = true }),
   pattern = "*",
   callback = function()
