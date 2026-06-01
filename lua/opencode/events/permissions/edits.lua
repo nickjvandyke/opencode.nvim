84761dd3c05da7d07b5b9365a8e0be5fb2493db1
Nick van Dyke <nickjvandyke15@gmail.com>
2026-06-01 08:30:48 -0600

    sync formatting

--- lua/opencode/events/permissions/edits.lua
+++ lua/opencode/events/permissions/edits.lua
@@ -49,8 +49,6 @@ end
 ---@param event opencode.server.Event
 ---@param server opencode.server.Server
 function M.diff(event, server)
-  local opts = require("opencode.config").opts.events.permissions or {}
-
   if event.type == "permission.asked" and event.properties.permission == "edit" then
     -- TODO: Handle multi-file edits?
     -- When would opencode even do that?
@@ -68,21 +66,21 @@ function M.diff(event, server)
       return
     end
 
-      local filepath = event.properties.metadata.filepath
+    local filepath = event.properties.metadata.filepath
 
-      -- Diffing changes some of the buffer's display options (namely folding) to make it easier to compare side-by-side,
-      -- so open the target file in a new tab first.
-      vim.cmd("tabnew " .. vim.fn.fnameescape(filepath))
-      -- FIX: Sometimes rejects? Or displays no changes? Particularly with a single inline change. Malformed patch?
-      vim.cmd("silent vert diffpatch " .. vim.fn.fnameescape(patch_filepath))
+    -- Diffing changes some of the buffer's display options (namely folding) to make it easier to compare side-by-side,
+    -- so open the target file in a new tab first.
+    vim.cmd("tabnew " .. vim.fn.fnameescape(filepath))
+    -- FIX: Sometimes rejects? Or displays no changes? Particularly with a single inline change. Malformed patch?
+    vim.cmd("silent vert diffpatch " .. vim.fn.fnameescape(patch_filepath))
 
-      diff_tabpage = vim.api.nvim_get_current_tabpage()
-      diff_new_buf = vim.api.nvim_get_current_buf()
-      register_diff_tab_cleanup()
+    diff_tabpage = vim.api.nvim_get_current_tabpage()
+    diff_new_buf = vim.api.nvim_get_current_buf()
+    register_diff_tab_cleanup()
 
-      vim.bo[diff_new_buf].buflisted = false
+    vim.bo[diff_new_buf].buflisted = false
 
-      current_edit_request_id = event.properties.id
+    current_edit_request_id = event.properties.id
 
     ---@param reply opencode.server.permission.Reply
     local function permit(reply)
@@ -91,35 +89,34 @@ function M.diff(event, server)
       end)
     end
 
-      -- Override native accept/reject keymaps to reject the edit as a whole first, if it hasn't been already
-      vim.keymap.set("n", "dp", function()
-        if current_edit_request_id then
-          -- Clear so we don't close the tabpage in the "permission.replied" handler
-          -- and user can continue accepting/rejecting individual hunks (and then close the tabpage manually)
-          current_edit_request_id = nil
-          permit("reject")
-        end
-        return "dp"
-      end, { buffer = true, desc = "Accept opencode edit hunk", expr = true })
-      vim.keymap.set("n", "do", function()
-        if current_edit_request_id then
-          current_edit_request_id = nil
-          permit("reject")
-        end
-        return "do"
-      end, { buffer = true, desc = "Reject opencode edit hunk", expr = true })
-      -- Accept/reject edit as a whole
-      vim.keymap.set("n", "da", function()
-        permit("once")
-      end, { buffer = true, desc = "Accept opencode edit" })
-      vim.keymap.set("n", "dr", function()
+    -- Override native accept/reject keymaps to reject the edit as a whole first, if it hasn't been already
+    vim.keymap.set("n", "dp", function()
+      if current_edit_request_id then
+        -- Clear so we don't close the tabpage in the "permission.replied" handler
+        -- and user can continue accepting/rejecting individual hunks (and then close the tabpage manually)
+        current_edit_request_id = nil
         permit("reject")
-      end, { buffer = true, desc = "Reject opencode edit" })
-      -- Close diff
-      vim.keymap.set("n", "q", function()
-        close_diff_tab()
-      end, { buffer = true, desc = "Close opencode edit diff" })
-    end)
+      end
+      return "dp"
+    end, { buffer = true, desc = "Accept opencode edit hunk", expr = true })
+    vim.keymap.set("n", "do", function()
+      if current_edit_request_id then
+        current_edit_request_id = nil
+        permit("reject")
+      end
+      return "do"
+    end, { buffer = true, desc = "Reject opencode edit hunk", expr = true })
+    -- Accept/reject edit as a whole
+    vim.keymap.set("n", "da", function()
+      permit("once")
+    end, { buffer = true, desc = "Accept opencode edit" })
+    vim.keymap.set("n", "dr", function()
+      permit("reject")
+    end, { buffer = true, desc = "Reject opencode edit" })
+    -- Close diff
+    vim.keymap.set("n", "q", function()
+      close_diff_tab()
+    end, { buffer = true, desc = "Close opencode edit diff" })
   elseif event.type == "permission.replied" and current_edit_request_id == event.properties.requestID then
     -- Entire edit was accepted or rejected, either in the plugin or TUI; close the diff
     close_diff_tab()
